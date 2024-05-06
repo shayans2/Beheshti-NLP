02d32389d0cfe0ee1e785ccea56804ce92db1215
zahra shariati <zahrashariati79@gmail.com>
2024-05-06 11:32:48 +0300

    made some changes in settings.py in the config folder

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,6 +1,7 @@
 # Application settings (e.g., model names or paths)
 NER_MODEL_NAME = "HooshvareLab/bert-base-parsbert-ner-uncased"
 PARAPHRASER_MODEL_NAME = "google/mt5-small"
-
+CLIP_MODEL_PATH = "openai/clip-vit-base-patch32"
+MT5_MODLE_PATH = "persiannlp/mt5-base-parsinlu-opus-translation_fa_en"
 
 PARAPHRASER_MODEL_PATH = "app/model_files/paraphraser.ckpt"
