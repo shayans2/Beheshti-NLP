9dee07ac7ced063ae3052c5e34c52850d06c63e9
Mohammad Mehdi Begmaz <Mohammadmehdi.begmaz@gmail.com>
2024-05-24 22:24:32 +0330

    Initial commit

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,7 +3,3 @@ NER_MODEL_NAME = "HooshvareLab/bert-base-parsbert-ner-uncased"
 PARAPHRASER_MODEL_NAME = "google/mt5-small"
 
 PARAPHRASER_MODEL_PATH = "app/model_files/paraphraser.ckpt"
-
-## Intent Classification Settings
-BERT_BASE_TOKENIZER = "HooshvareLab/bert-base-parsbert-uncased"
-BERT_BASE_MODEL = "HooshvareLab/bert-base-parsbert-uncased"
--- app/schemas.py
+++ app/schemas.py
@@ -1,24 +1,7 @@
 from pydantic import BaseModel
-from typing import Dict
 
 class NERSchema(BaseModel):
     query: str
 
 class ParaphraserSchema(BaseModel):
     query: str
-
-class IntentSchema(BaseModel):
-    """
-    Schema for intent classification input using Pydantic.
-
-    This schema defines the structure of the input data required for intent classification.
-    
-    Attributes:
-    ----------
-    query : str
-        The input sentence to classify.
-    data : Dict
-        A dictionary where keys are intent labels and values are lists of example sentences.
-    """
-    query: str
-    data: Dict
