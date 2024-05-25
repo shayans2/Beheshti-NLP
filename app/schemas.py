e63955439ca0b4e517197f867dfd8c43e44b4b06
Mahdieh Sajedipoor <mahdiehsajedipoor@gmail.com>
2024-05-25 21:51:02 +0330

    docs(keyword extraction): add schema

--- app/schemas.py
+++ app/schemas.py
@@ -1,18 +1,21 @@
 from pydantic import BaseModel
 from typing import Dict
 
+
 class NERSchema(BaseModel):
     query: str
 
+
 class ParaphraserSchema(BaseModel):
     query: str
+
 
 class IntentSchema(BaseModel):
     """
     Schema for intent classification input using Pydantic.
 
     This schema defines the structure of the input data required for intent classification.
-    
+
     Attributes:
     ----------
     query : str
@@ -20,5 +23,11 @@ class IntentSchema(BaseModel):
     data : Dict
         A dictionary where keys are intent labels and values are lists of example sentences.
     """
+
     query: str
     data: Dict
+
+
+class KeywordExtractionSchema(BaseModel):
+    query: str
+    top_n: int = 10
