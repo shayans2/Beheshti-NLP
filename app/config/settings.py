d9cc1bd8ee1e302d6518ab1d085b1a5231891f49
Mahdieh Sajedipoor <mahdiehsajedipoor@gmail.com>
2024-05-25 21:46:38 +0330

    feat(keyword extraction): add service

--- app/config/settings.py
+++ app/config/settings.py
@@ -7,3 +7,6 @@ PARAPHRASER_MODEL_PATH = "app/model_files/paraphraser.ckpt"
 ## Intent Classification Settings
 BERT_BASE_TOKENIZER = "HooshvareLab/bert-base-parsbert-uncased"
 BERT_BASE_MODEL = "HooshvareLab/bert-base-parsbert-uncased"
+
+POS_TAGGER_MODEL = "app/model_files/pos_tagger.model"
+TF_IDF_MODEL = "app/model_files/tf-idf-model.pkl"
